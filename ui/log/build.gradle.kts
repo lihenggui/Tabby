351739135f668fa9a923bdb1d1d159797e9c3c18
Mercury Li <contact@mercury.li>
2026-06-05 21:58:47 -0700

    Move log Android wrappers to androidMain

--- ui/log/build.gradle.kts
+++ ui/log/build.gradle.kts
@@ -15,8 +15,6 @@ kotlin {
     }
 
     androidMain {
-      kotlin.srcDir("src/main/kotlin")
-
       dependencies {
         implementation(projects.core.engineAndroid)
         implementation(projects.glue)
